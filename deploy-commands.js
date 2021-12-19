e6c09880b59c2b217d1bea11aea4ed82271dd23e
liamodo <88376295+liamodo@users.noreply.github.com>
2021-12-19 21:59:46 +0100

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -17,11 +17,9 @@ const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
 	try {
 		console.log('Started refreshing application (/) commands.');
 		await rest.put(
-			Routes.applicationGuildCommands(process.env.CLIENT_ID),{ body: commands },);
+			Routes.applicationCommands(process.env.CLIENT_ID),{ body: commands },);
 		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
+	} catch (error) {console.error(error);}
 })();
 
 //HOW GET GUILD ID?
